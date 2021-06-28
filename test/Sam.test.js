417f2a6dfd7e55cfc8ecb7c3ad109b28b5f0bf0e
Spencer Colton <me@scolton.tech>
2021-06-28 14:37:26 -0500

    Initial commit

---
+++ test/Sam.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('sam', function() {
+  it('is not a rapidly rotating bladed object', function() {
+    assert(typeof(sam) !== 'rapidly rotating bladed object');
+  });
+});
